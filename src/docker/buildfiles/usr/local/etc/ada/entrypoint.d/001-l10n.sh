345345102b743a962f365537c94c8ad527368eca
Ralf Zimmermann <hello@waldhacker.dev>
2023-01-10 11:46:23 +0100

    [TASK] Set timezone first #SGN-294

--- src/docker/buildfiles/usr/local/etc/ada/entrypoint.d/001-l10n.sh
+++ src/docker/buildfiles/usr/local/etc/ada/entrypoint.d/001-l10n.sh
@@ -5,6 +5,21 @@
 # **********************************************
 
 setup_language() {
+    if [ -f "/home/ada/.local/share/ada/timezone" ]; then
+        local ADA_TIME=$(cat /home/ada/.local/share/ada/timezone | tr -d '\n')
+    fi
+
+    if [ ! -z "ADA_TIME" ]; then
+        if [ ! -z "$ADA_VERBOSE" ]; then echo "use timezone $ADA_TIME"; fi
+
+        echo $ADA_TIME > /etc/timezone
+        if [ -f "/usr/share/zoneinfo/$ADA_TIME" ]; then
+            cp "/usr/share/zoneinfo/$ADA_TIME" /etc/localtime
+        fi
+
+        export TZ=$ADA_TIME
+    fi
+
     if [ ! -f "/home/ada/.local/share/ada/lang" ]; then
         return 0
     fi
@@ -20,20 +35,6 @@ setup_language() {
     export LANG=$ADA_LANG
     export LANGUAGE=$ADA_LANG
     export LC_ALL=$ADA_LANG
-
-    if [ -f "/home/ada/.local/share/ada/timezone" ]; then
-        local ADA_TIME=$(cat /home/ada/.local/share/ada/timezone | tr -d '\n')
-    fi
-
-    if [ ! -z "ADA_TIME" ]; then
-        if [ ! -z "$ADA_VERBOSE" ]; then echo "use timezone $ADA_TIME"; fi
-        export TZ=$ADA_TIME
-
-        echo $ADA_TIME > /etc/timezone
-        if [ -f "/usr/share/zoneinfo/$ADA_TIME" ]; then
-            cp "/usr/share/zoneinfo/$ADA_TIME" /etc/localtime
-        fi
-    fi
 }
 
 setup_language
